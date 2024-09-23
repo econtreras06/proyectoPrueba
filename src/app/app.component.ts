4b280c1c8ccc12bf096c0592a8e60c9596bec09e
Elen <econtreras@ideasoftwaresas.com>
2024-09-23 18:20:50 -0400

    data binding ejemplos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = "proyectoPrueba";
 
+  nombre: string = "Elen";
+  edad: number = 27;
+  persona = {
+    nombre: 'Juanito',
+    edad: 15
+  }
+  imagen = './../assets/imagenPrueba.png'
 
+  isDisabled: boolean = false;
 }
